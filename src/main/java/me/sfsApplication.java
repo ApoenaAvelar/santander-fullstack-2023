0993d3c9dfa80c5afc9f10b9453ffc84c395bb72
ApoenaAvelar <apoenajorge@gmail.com>
2023-10-16 12:45:47 -0300

    Implementada a camada de dominio

---
+++ src/main/java/me/sfsApplication.java
@@ -0,0 +1,13 @@
+package me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class sfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(sfsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/SantanderFullstack2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderFullstack2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
