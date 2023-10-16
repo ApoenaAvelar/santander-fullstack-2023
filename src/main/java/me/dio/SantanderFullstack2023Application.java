c9078c37e6044a7c92432bfe953a95a987be1bf2
ApoenaAvelar <apoenajorge@gmail.com>
2023-10-16 12:45:47 -0300

    Initial commit

---
+++ src/main/java/me/dio/SantanderFullstack2023Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderFullstack2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderFullstack2023Application.class, args);
+	}
+
+}
